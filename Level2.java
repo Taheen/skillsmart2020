2ed2d7adcd807c511a796c75e7993371e266ddad
Denissio <dennistorhov@gmail.com>
2020-01-20 12:03:02 +0000

    Update Level2.java

--- Level2.java
+++ Level2.java
@@ -6,10 +6,15 @@ public class Level1 {
     int N = oksana.length;
 	int distancePerHour = 0;
 	int distanceTotal = 0;
-	  for (int i = 0; i < N; i += 2) {
-		distancePerHour = oksana[i] * oksana[i + 1];
-		distanceTotal = distanceTotal + distancePerHour;
+	for (int i = 0; i < N; i += 2) {
+		if (i < 2) {
+			distancePerHour = oksana[i] * oksana[i + 1];
+		  }
+		else {
+			distancePerHour = oksana[i] * (oksana[i + 1] - oksana[i - 1]);
+		  }
+		distanceTotal += distancePerHour;
 		}
-	return distanceTotal;
+		return distanceTotal;
   }
 }
