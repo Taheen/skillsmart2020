4b276032b067f94bc7bf506703223450353cbc1a
Dennis T <dennistorhov@gmail.com>
2020-12-31 15:01:43 +0000

    Update Level1.java

--- current files/Level1.java
+++ current files/Level1.java
@@ -11,6 +11,7 @@ public class Level1 {
         String pattern = line.substring(0, marker);
         StringBuilder sb1 = new StringBuilder(line);
         sb1.delete(0, marker);
+        System.out.println("pattern: " + pattern);
 
         // if no dots present
         if (!(line.contains("."))){
@@ -21,10 +22,11 @@ public class Level1 {
             match = true;
         }
 
-        for (int i = 0; i < sb1.length() - 1; i++){
+        for (int i = 0; i < sb1.length(); i++){
 
             if (sb1.indexOf(pattern) == 0){
                 sb1.delete(0, marker);
+                System.out.println("remaining string: " + sb1);
 
                 //remaining string shorter than pattern, no match
                 if (sb1.length() > 1 && sb1.length() <= pattern.length()){
