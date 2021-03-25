f443db60068462ee7c5c3901bd468f3ae5aa519d
Dennis T <dennistorhov@gmail.com>
2021-03-25 10:54:43 +0000

    Matrix rotation solved

--- older files/MatrixRotation/MatrixRotation.java
+++ older files/MatrixRotation/MatrixRotation.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class Level1 {
+public class MatrixRotation {
 
     public static void MatrixTurn(String Matrix[], int M, int N, int T) {
 
@@ -20,28 +20,51 @@ public class Level1 {
 
         //  ---- main loop (number of rotations) ----
         for (int s = 0; s < T; s++){
-
+            System.out.println("rotation step " + s);
             // ---- inner loop (rotating rings starting from outer ring) ----
             for (int r = 0; r < ringAmount; r++){
-                rotationClockwise(testArr, currentRingBorders(testArr, r));
-            }
+                System.out.println("rotating ring " + r);
 
+                rotationClockwise(testArr, currentRingBorders(testArr, r));
+
+            }
+            System.out.println("===== matrix after rotaion " + s + " =========");
+            printArray(testArr);
+            System.out.println("----------------------");
         }
 
         for (int i = 0; i < Matrix.length; i++){
             Matrix[i] = new String(testArr[i]);
         }
 
+        System.out.println(Arrays.toString(Matrix));
     }
 
     //ring coordinates(top left corner, height, length)
     public static int[] currentRingBorders (char[][] inputArr1, int ring){
         int[] borders = new int[3];
         int s = ring;
+        System.out.println("coordinates: offset, height, length");
+
         borders[0] = s;
         borders[1] = inputArr1.length - 1 - s;
         borders[2] = inputArr1[0].length - 1 - s;
+
+        for (int i = 0; i < 3; i++){
+            System.out.print(borders[i] + " ");
+        }
+        System.out.println();
         return borders;
+    }
+
+    public static void printArray (char[][] arr2print){
+        for (int j = 0; j < arr2print.length; j++) {
+            for (int i = 0; i < arr2print[0].length; i++) {
+                System.out.print(arr2print[j][i]);
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
     }
 
     public static void rotationClockwise (char[][] arr2rotate, int[] borders){
@@ -68,5 +91,24 @@ public class Level1 {
             arr2rotate[i][borders[2]] = arr2rotate[i - 1][borders[2]];
         }
         arr2rotate[borders[0] + 1][borders[2]] = temp;
+    }
+
+    public static void main(String[] args) {
+        String[] test1 = {"123456", "234567", "345678", "456789"};
+        int testM = 4;
+        int testN = 6;
+        int testT = 3;
+
+        char[][] testArr = new char[testM][testN];
+        for (int i = 0; i < testM; i++){
+            for (int j = 0; j < testN; j++){
+                testArr[i][j] = test1[i].charAt(j);
+            }
+        }
+
+        printArray(testArr);
+
+        MatrixTurn(test1, testM,testN,testT);
+
     }
 }
