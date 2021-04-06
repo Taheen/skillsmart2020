8ead2df1c790d9b4617c3ed3d25056b93cd7a6b6
Dennis T <dennistorhov@gmail.com>
2021-04-06 20:41:35 +0100

    Chubaka solved

--- current files/Level1.java
+++ current files/Level1.java
@@ -3,6 +3,46 @@ import java.util.*;
 public class Level1 {
 
     boolean TransformTransform(int A[], int N) {
-        return true;
+            int checkSum = 0;
+            ArrayList<Integer> list1 = new ArrayList<>();
+            ArrayList<Integer> list3;
+            for (int b = 0; b < A.length; b++){
+                list1.add(A[b]);
+            }
+
+            list3 = transform1(transform1(list1));
+
+            for (int c = 0; c < list3.size(); c++){
+                checkSum = checkSum + list3.get(c);
+            }
+            if (checkSum % 2 == 0) {
+                return true;
+            }
+            else return false;
     }
+
+    public static ArrayList<Integer> transform1(ArrayList<Integer> inputList){
+        int k;
+        int max;
+        ArrayList<Integer> transformedList = new ArrayList<>();
+
+        for (int i = 0; i <= inputList.size() - 1; i++){
+            for (int j = 0; j <= inputList.size() - i - 1; j++){
+                k = i + j;
+                if (k - j == 0) {
+                    transformedList.add(inputList.get(0));
+                }
+                else {
+                    max = inputList.get(j);
+                    for (int a = j; a <= k; a++){
+                        if(inputList.get(a) > max) max = inputList.get(a);
+                    }
+                        transformedList.add(max);
+                }
+            }
+
+        }
+        return transformedList;
+    }
+
 }
---
+++ older files/Chubaka/Transform.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+
+public class Transform {
+
+    public static boolean TransformTransform(int A[], int N){
+        int checkSum = 0;
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list3;
+        for (int b = 0; b < A.length; b++){
+            list1.add(A[b]);
+        }
+
+        list3 = transform1(transform1(list1));
+
+        for (int c = 0; c < list3.size(); c++){
+            checkSum = checkSum + list3.get(c);
+        }
+        System.out.println(checkSum);
+
+        if (checkSum % 2 == 0) {
+            return true;
+        }
+        else return false;
+    }
+
+    public static ArrayList<Integer> transform1(ArrayList<Integer> inputList){
+        int k;
+        int max;
+        ArrayList<Integer> transformedList = new ArrayList<>();
+
+        for (int i = 0; i <= inputList.size() - 1; i++){
+            for (int j = 0; j <= inputList.size() - i - 1; j++){
+                k = i + j;
+                if (k - j == 0) {
+                    transformedList.add(inputList.get(0));
+                }
+                else {
+                    max = inputList.get(j);
+                    for (int a = j; a <= k; a++){
+                        if(inputList.get(a) > max) max = inputList.get(a);
+                    }
+                    transformedList.add(max);
+                }
+            }
+
+        }
+        return transformedList;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int[] testArr = {1,2,6};
+
+
+       System.out.println(TransformTransform(testArr, 0));
+
+
+    }
+}
